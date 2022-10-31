c60a0577a8edad1c61d272645bd52dce34c3f806
Bruce Wen <bwen@uchicago.edu>
2022-10-31 14:14:26 -0500

    edits

--- sotheby_data/with_N_bidders/estimate_sotheby_withN.py
+++ sotheby_data/with_N_bidders/estimate_sotheby_withN.py
@@ -8,14 +8,14 @@ import sys
 VAL = "withN"
 
 
-with open('output_dicts_jump2_{}.pickle'.format(VAL), 'rb') as handle:
-    output_dicts_jump2 = pickle.load(handle)
+# with open('output_dicts_jump2_{}.pickle'.format(VAL), 'rb') as handle:
+#     output_dicts_jump2 = pickle.load(handle)
 
 # with open('output_dicts_NOjump2_{}.pickle'.format(VAL), 'rb') as handle:
 #     output_dicts_NOjump2 = pickle.load(handle)
 
-# with open('output_dicts_ALL_{}.pickle'.format(VAL), 'rb') as handle:
-#     output_dicts_ALL = pickle.load(handle)
+with open('output_dicts_ALL_{}.pickle'.format(VAL), 'rb') as handle:
+    output_dicts_ALL = pickle.load(handle)
 
 
 # print(output_dicts_ALL)
@@ -31,14 +31,14 @@ def estimate_F(X, data):
         F_L_array.append(F_L_v)
     return F_U_array, F_L_array
 
-# all_F_U, all_F_L = estimate_F(X, output_dicts_ALL)
-all_F_U_jump2, all_F_L_jump2 = estimate_F(X, output_dicts_jump2)
+all_F_U, all_F_L = estimate_F(X, output_dicts_ALL)
+# all_F_U_jump2, all_F_L_jump2 = estimate_F(X, output_dicts_jump2)
 # all_F_U_NOjump2, all_F_L_NOjump2 = estimate_F(X, output_dicts_NOjump2)
 
-# plt.scatter(X,all_F_U,color='orange', label='upper bound')
-# plt.scatter(X,all_F_L,color='blue', label='lower bound')
-plt.scatter(X,all_F_U_jump2,color='red', label='jump2_upper')
-plt.scatter(X,all_F_L_jump2,color='green', label='jump2_lower')
+plt.scatter(X,all_F_U,color='orange', label='upper bound')
+plt.scatter(X,all_F_L,color='blue', label='lower bound')
+# plt.scatter(X,all_F_U_jump2,color='red', label='jump2_upper')
+# plt.scatter(X,all_F_L_jump2,color='green', label='jump2_lower')
 # plt.scatter(X,all_F_U_NOjump2,color='purple', label='nojump_upper')
 # plt.scatter(X,all_F_L_NOjump2,color='yellow', label='nojump_lower')
 plt.legend()
